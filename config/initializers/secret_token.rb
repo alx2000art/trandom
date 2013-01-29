b6d16b821f57392c026e5de5668adb0e97824fdd
Ubuntu_Ruby <rubystack@ubuntu.(none)>
2013-01-29 07:39:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trandom::Application.config.secret_token = 'd83871acb22ffaf31f47cbd5874b27dcdb2b038fc2572c378ab619a633f2f5b7e531e3181c6a4d773a8af62277310b55f09083dd14565766c85269c70be87d06'
